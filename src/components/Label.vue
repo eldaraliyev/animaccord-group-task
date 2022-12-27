07640e0e2e699dcffec6ad53b7d8f2f9cf0b0662
eldaralv <eldar.alv@protonmail.com>
2022-12-27 10:54:12 +0400

    feat: button label init, styling

---
+++ src/components/Label.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, defineProps } from "vue";
+const props = defineProps({
+  title: String,
+});
+
+const title = computed(() => props.title)
+</script>
+
+<template>
+  <p class="label">{{ title }}</p>
+</template>
+
+<style lang="scss" scoped>
+.label {
+  text-align: left;
+  text-transform: uppercase;
+}
+</style>
